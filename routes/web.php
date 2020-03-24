214b075cf16e0889a1aa3fa8a1a1b2da3ed5791f
vytautas <vytautas.rimeikis@gmail.com>
2020-03-24 18:44:59 +0200

    add laravel ui package

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
