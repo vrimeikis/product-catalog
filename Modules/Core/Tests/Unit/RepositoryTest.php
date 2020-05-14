bce11405934167335d00d278d551ac46bad41026
vytautas <vytautas.rimeikis@gmail.com>
2020-05-14 21:01:39 +0300

    repository tests

--- Modules/Core/Tests/Unit/RepositoryTest.php
+++ Modules/Core/Tests/Unit/RepositoryTest.php
@@ -6,7 +6,9 @@ namespace Modules\Core\Tests\Unit;
 
 use Faker\Generator;
 use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Collection;
 use Modules\Core\Repositories\Repository;
@@ -80,8 +82,10 @@ class RepositoryTest extends RepositoryTestCase
         $users = factory(User::class, $count)->create();
         $testRepository = $this->getTestClassInstance();
 
+        // #1
         $this->assertCount($count, $testRepository->all());
 
+        // #2
         $testRepository->all()->each(function (User $user) use ($users, &$i) {
             $factoryUser = $users->get($i++);
 
@@ -91,6 +95,7 @@ class RepositoryTest extends RepositoryTestCase
         });
 
         $i = 0;
+        // #3
         $testRepository->all(['name'])->each(function (User $user) use ($users, &$i) {
             /** @var User $factoryUser */
             $factoryUser = $users->get($i++);
@@ -98,6 +103,222 @@ class RepositoryTest extends RepositoryTestCase
             $this->assertEquals($factoryUser->name, $user->name);
             $this->assertEquals(['name'], array_keys($user->getAttributes()));
         });
+    }
+
+    /**
+     * @group repository
+     * @throws BindingResolutionException
+     */
+    public function testFindReturnNullNonExistsRecord(): void
+    {
+        $id = mt_rand(1, 10);
+
+        $this->assertNull($this->getTestClassInstance()->find($id));
+    }
+
+    /**
+     * @group repository
+     * @throws BindingResolutionException
+     */
+    public function testFindReturnFilledModel(): void
+    {
+        factory(User::class, 2)->create();
+
+        /** @var User $user */
+        $user = factory(User::class)->create();
+
+        factory(User::class, 3)->create();
+
+        $dbUser = $this->getTestClassInstance()->find($user->id);
+
+        $this->assertNotNull($dbUser);
+        $this->assertInstanceOf(User::class, $dbUser);
+        $this->assertEquals($user->getAttributes(), $dbUser->getAttributes());
+    }
+
+    /**
+     * @group repository
+     * @throws BindingResolutionException
+     */
+    public function testFindWithRelation(): void
+    {
+        /** @var Role $role */
+        $role = factory(Role::class)->create();
+
+        /** @var User $user */
+        $user = factory(User::class)->create([
+            'role_id' => $role->id,
+        ]);
+
+        $result = $this->getTestClassInstance()->with(['role'])->find($user->id);
+
+        $this->assertTrue($result->relationLoaded('role'));
+    }
+
+    /**
+     * @group repository
+     * @throws BindingResolutionException
+     */
+    public function testFindOrFailThrowException(): void
+    {
+        $id = mt_rand(1, 10);
+
+        $this->expectException(ModelNotFoundException::class);
+        $this->getTestClassInstance()->findOrFail($id);
+    }
+
+    /**
+     * @group repository
+     * @throws BindingResolutionException
+     */
+    public function testFindOrFailReturnModel(): void
+    {
+        factory(User::class, 3)->create();
+
+        /** @var User $user */
+        $user = factory(User::class)->create();
+
+        $dbUser = $this->getTestClassInstance()->findOrFail($user->id);
+
+        $this->assertNotNull($dbUser);
+        $this->assertInstanceOf(User::class, $dbUser);
+        $this->assertEquals($user->getAttributes(), $dbUser->getAttributes());
+    }
+
+    /**
+     * @group repository
+     * @throws BindingResolutionException
+     */
+    public function testPluckOnEmptyTable(): void
+    {
+        $result = $this->getTestClassInstance()->pluck('name');
+
+        $this->assertEmpty($result);
+        $this->assertInstanceOf(Collection::class, $result);
+    }
+
+    /**
+     * @group repository
+     * @throws BindingResolutionException
+     */
+    public function testPluckSetOnlyColumn(): void
+    {
+        /** @var Collection $users */
+        $users = factory(User::class, 3)->create();
+
+        $result = $this->getTestClassInstance()->pluck('name');
+
+        $this->assertEquals(
+            $users->pluck('name'),
+            $result
+        );
+    }
+
+    /**
+     * @group repository
+     * @throws BindingResolutionException
+     */
+    public function testPluckSetColumnAndKey(): void
+    {
+        /** @var Collection $users */
+        $users = factory(User::class, 3)->create();
+        $result = $this->getTestClassInstance()->pluck('name', 'id');
+
+        $this->assertEquals(
+            $users->pluck('name', 'id'),
+            $result
+        );
+    }
+
+    /**
+     * @group repository
+     * @throws BindingResolutionException
+     */
+    public function testCreate(): void
+    {
+        $data = [
+            'name' => 'test_name',
+            'email' => 'test_email',
+            'password' => 'secret',
+        ];
+
+        /** @var User $fakeUser */
+        $fakeUser = $this->getTestClassInstance()->create($data);
+
+        $this->assertInstanceOf(User::class, $fakeUser);
+        $this->assertEquals($data['name'], $fakeUser->name);
+        $this->assertEquals($data['email'], $fakeUser->email);
+        $this->assertEquals($data['password'], $fakeUser->password);
+
+        $this->assertDatabaseHas('users', $data);
+    }
+
+    /**
+     * @group repository
+     * @throws BindingResolutionException
+     */
+    public function testPaginateOnEmptyTable(): void
+    {
+        $paginateResult = $this->getTestClassInstance()->paginate();
+
+        $this->assertInstanceOf(LengthAwarePaginator::class, $paginateResult);
+        $this->assertTrue($paginateResult->isEmpty());
+    }
+
+    /**
+     * @group repository
+     * @throws BindingResolutionException
+     */
+    public function testPaginateNonEmptyTable(): void
+    {
+        $numberPerPage = mt_rand(1, 30);
+        $count = mt_rand(2, 100);
+
+        /** @var Collection|User[] $users */
+        $users = factory(User::class, $count)->create();
+
+        /** @var LengthAwarePaginator|User[] $paginator */
+        $paginator = $this->getTestClassInstance()->paginate($numberPerPage);
+
+        $this->assertEquals($users[0]->toArray(), $paginator[0]->toArray());
+        $this->assertInstanceOf(LengthAwarePaginator::class, $paginator);
+        $this->assertEquals($numberPerPage, $paginator->perPage());
+        $this->assertEquals($count, $paginator->total());
+    }
+
+    /**
+     * @group repository
+     * @throws BindingResolutionException
+     */
+    public function testUpdate(): void
+    {
+        /** @var User $user */
+        $user = factory(User::class)->create([
+            'name' => 'Denis'
+        ]);
+
+        $this->getTestClassInstance()->update(['name' => 'John'], $user->id);
+
+        $this->assertDatabaseHas('users', ['id' => $user->id, 'name' => 'John']);
+        $this->assertDatabaseMissing('users', ['id' => $user->id, 'name' => 'Denis']);
+    }
+
+    /**
+     * @group repository
+     * @throws BindingResolutionException
+     */
+    public function testDelete(): void
+    {
+        /** @var User $user */
+        $user = factory(User::class)->create();
+
+        /** @var User $otherUser */
+        $otherUser = factory(User::class)->create();
+
+        $this->getTestClassInstance()->delete($user->id);
+
+        $this->assertDatabaseMissing('users', ['id' => $user->id]);
+        $this->assertDatabaseHas('users', ['id' => $otherUser->id]);
     }
 
     /**
@@ -113,7 +334,7 @@ class RepositoryTest extends RepositoryTestCase
 /**
  * Class User
  * @package Modules\Core\Tests\Unit
- *
+ * @property int $id
  * @property string $name
  * @property string $email
  * @property string $password
@@ -129,6 +350,7 @@ class User extends Model
         'name',
         'email',
         'password',
+        'remember_token',
         'role_id',
     ];
 
@@ -144,6 +366,7 @@ class User extends Model
 /**
  * Class Role
  * @package Modules\Core\Tests\Unit
+ * @property int $id
  * @property string $name
  */
 class Role extends Model
