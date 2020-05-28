cd750521bc6d1f0a72a12f61a59df86d872c4ed0
vytautas <vytautas.rimeikis@gmail.com>
2020-05-28 20:05:40 +0300

    set private api middleware to api routes

--- Modules/ContactUs/Routes/api.php
+++ Modules/ContactUs/Routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+declare(strict_types = 1);
+
 use Illuminate\Support\Facades\Route;
+use Modules\Api\Http\Middleware\ApiPrivateMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::namespace('API')->name('api.')->group(function () {
+Route::middleware(ApiPrivateMiddleware::class)->namespace('API')->name('api.')->group(function () {
     Route::apiResource('contact-us', 'ContactMessageController')->only(['store']);
 });
--- Modules/Customer/Routes/api.php
+++ Modules/Customer/Routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+declare(strict_types = 1);
+
 use Illuminate\Support\Facades\Route;
+use Modules\Api\Http\Middleware\ApiPrivateMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::namespace('API')->name('api.')->group(function () {
+Route::middleware(ApiPrivateMiddleware::class)->namespace('API')->name('api.')->group(function () {
     Route::prefix('auth')->namespace('Auth')->group(function () {
         Route::post('register', 'AuthenticationController@register')->name('register');
         Route::post('login', 'AuthenticationController@login')->name('login');
